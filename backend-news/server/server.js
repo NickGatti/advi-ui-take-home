bd55dc11034927415ff7bfa1eb5a3601bff34c03
Nick Gatti <nick.gatti@gmail.com>
2023-04-14 14:01:08 -0700

    added dev api

--- backend-news/server/server.js
+++ backend-news/server/server.js
@@ -3,10 +3,16 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+const teslaNews = require('./dev/teslaNews.json')
+
 app.use(
     '/',
     express.static(path.join(__dirname, '../dist'))
   );
+
+app.get('/dev/news', (req, res) => {
+    res.json(teslaNews)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
