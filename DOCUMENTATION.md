3baa0df67a05993313ea7a201c835377b4f3d35e
Nick Gatti <nick.gatti@gmail.com>
2023-04-18 12:21:02 -0700

    summary cut off in card

--- DOCUMENTATION.md
+++ DOCUMENTATION.md
@@ -23,7 +23,8 @@ I am using both Windows and Linux for dev. Which will explain the forward and ba
 ## Dev mode
 > To run just the server with `nodemon` run the command: `npx nodemon server/server.js` in `\backend-news`
 
-> To run the server with `nodemon` and have the front-end on filewatch, run this command along with `nodemon` running: `npm run watch` in `\frontend-news`
-* EX: run `npx nodemon server/server.js` in `\backend-news` then run `npm run watch` in `\frontend-news`
+> To run the server with `nodemon` and have the front-end on filewatch, with the `nodemon` server already running, run the command: `npm run watch` in `\frontend-news`
+
+* EX: run `npx nodemon server/server.js` in `\backend-news` then run `npm run watch` in `\frontend-news` each in a separate shell instance
 
 > If you arent deving on the server, close `nodemon` and run just `node` for the server, run the command: `node server/server.js` in `\backend-news` this will make the filewatch performance a little better
--- frontend-news/src/components/newsCard/NewsCard.js
+++ frontend-news/src/components/newsCard/NewsCard.js
@@ -1,17 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const NewsCard = ({ article: {
     title,
     pubDate,
     sentiment,
-    url,
-    summary,
+    url = '',
+    summary = '',
     imageUrl,
 } } ) => {
+    const [reducedSummary, setReducedSummary] = useState(summary)
+    const [reducedUrl, setReducedUrl] = useState(url)
 
     useEffect(() => {
-        // console.log('sentiment', sentiment)
-    }, [])
+        if (summary.length > 250) {
+            let newSummary = reducedSummary
+            setReducedSummary(newSummary.slice(0, 250) + '...')
+        }
+        if (url.length > 72) {
+            let newUrl = reducedUrl
+            setReducedUrl(newUrl.slice(0, 72) + '...')
+        }
+    }, [summary, reducedSummary, url, reducedUrl])
 
     /*
     { 
@@ -37,8 +46,8 @@ const NewsCard = ({ article: {
                 <div className='divider'>
                     <h4 className="cardTitle">{title}</h4>
                     <p className="cardDate">{new Date(pubDate).toString()}</p>
-                    {summary ? <p className="cardSummary">{summary}</p> : null}
-                    <a className="cardAnchor" href={url}>{url}</a>
+                    {reducedSummary ? <p className="cardSummary">{reducedSummary}</p> : null}
+                    <a className="cardAnchor" href={url}>{reducedUrl}</a>
                 </div>
                 <img className='cardImage' src={imageUrl} alt={title} />
             </div>
